bc8ec69c47b22be9c75306624f80afbf328eb5f6
Bruno214 <bruno214215216217218@gmail.com>
2024-05-04 19:56:35 -0300

    feat: criado a branch de desenvolvimento

--- src/main/java/com/cederj/uff/tcc/stockmaster/StockmasterApplication.java
+++ src/main/java/com/cederj/uff/tcc/stockmaster/StockmasterApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StockmasterApplication {
 
 	public static void main(String[] args) {
+		// branch de desenvolvimento
 		SpringApplication.run(StockmasterApplication.class, args);
 	}
 
